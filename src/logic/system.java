465b4744d58d1a17eaf1f709222a4c15faa86e68
Alejandro <alitoxcr@gmail.com>
2021-08-03 00:56:05 -0600

    Initial commit

---
+++ src/logic/system.java
@@ -0,0 +1,52 @@
+package logic;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class system {
+    Scanner input = new Scanner(System.in);
+    data_gestor dataGestor = new data_gestor();
+    String player;
+    Tiempo timerSystem = new Tiempo();
+
+    public void mainMenu(int opcion) throws FileNotFoundException {
+
+        switch (opcion){
+            case 1:
+                timerSystem.Contar();
+                dataGestor.cargarDatos();
+                dataGestor.imprimirDatos();
+                timerSystem.Detener();
+                System.out.println("Tiempo tardado en la consulta: " + timerSystem.getSegundos() + " segundos");
+
+                break;
+            case 2:
+                System.out.println("Digite el nombre del jugador");
+                player = input.nextLine();
+                timerSystem.Contar();
+                dataGestor.BuscarNamePlayer(player);
+                System.out.println("Tiempo tardado en la consulta:" + timerSystem.getSegundos() + " segundos");
+                break;
+            case 3:
+                System.out.println("Digite la estatura, edad o peso del jugador:");
+                player = input.nextLine();
+                timerSystem.Contar();
+                dataGestor.BuscarEdadPesoEstaturaPlayer(player);
+                System.out.println("Tiempo tardado en la consulta:" + timerSystem.getSegundos() + " segundos");
+                break;
+            case 4:
+                System.out.println("Digite la nacionalidad que quieres buscar:");
+                player = input.nextLine();
+                timerSystem.Contar();
+                dataGestor.BuscarPaisPlayer(player);
+                System.out.println("Tiempo tardado en la consulta:" + timerSystem.getSegundos() + " segundos");
+                break;
+            case 5:
+                System.out.println("Digite el dorsal del jugador:");
+                player = input.nextLine();
+                timerSystem.Contar();
+                dataGestor.BuscarDorsalPlayer(player);
+                System.out.println("Tiempo tardado en la consulta:" + timerSystem.getSegundos() + " segundos");
+        }
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,19 @@
+import ui.msg;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+import logic.system;
+public class main {
+    public static void main(String args[]) throws FileNotFoundException {
+
+        Scanner input = new Scanner(System.in);
+        msg message = new msg();
+        system system = new system();
+        int opcion = 0;
+        do{
+            System.out.println(message.menuPrincipal());
+            opcion = input.nextInt();
+            system.mainMenu(opcion);
+        }while(opcion!=6);
+    }
+}
